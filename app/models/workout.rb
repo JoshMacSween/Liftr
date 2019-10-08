78ee22efd985ba386c7ab71c9a1302dc87bff132
Joshua MacSween <joshmacsween@gmail.com>
2019-10-08 15:10:16 -0600

    working nested forms

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,4 @@
 class Workout < ApplicationRecord
-  has_many :exercises 
+  has_many :exercises, inverse_of: :workout
+  accepts_nested_attributes_for :exercises, reject_if: :all_blank, allow_destroy: true
 end
