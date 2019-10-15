0ae7e4839274fae751e79212f3c5372aff4342ad
Joshua MacSween <joshmacsween@gmail.com>
2019-10-15 15:08:21 -0600

    added user_id to workouts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_204204) do
+ActiveRecord::Schema.define(version: 2019_10_15_210613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_204204) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.datetime "date"
+    t.integer "user_id"
   end
 
 end
