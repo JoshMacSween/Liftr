9b4688d6672c238ad1a995c1720c9a85b1946f0f
Joshua MacSween <joshmacsween@gmail.com>
2019-10-16 17:39:39 -0600

    added validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,7 @@
 class Exercise < ApplicationRecord
   belongs_to :workout
+  validates :movement, presence: true
+  validates :weight, presence: true
+  validates :sets, presence: true
+  validates :reps, presence: true
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,6 @@ class Workout < ApplicationRecord
   has_many :exercises, inverse_of: :workout
   accepts_nested_attributes_for :exercises, reject_if: :all_blank, allow_destroy: true
   belongs_to :user
+  validates :title, presence: true
+  validates :date, presence: true
 end
