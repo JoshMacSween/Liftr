2c9a9bfc819ff9527b39a2505a1cdaa873664404
Joshua MacSween <joshmacsween@gmail.com>
2019-10-16 10:18:06 -0600

    has_many/belongs_to

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,5 @@
 class Workout < ApplicationRecord
   has_many :exercises, inverse_of: :workout
   accepts_nested_attributes_for :exercises, reject_if: :all_blank, allow_destroy: true
-
+  belongs_to :user
 end
