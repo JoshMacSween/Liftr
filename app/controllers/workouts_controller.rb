cc53478899f5fcdc23e0350d27e01b91231b671e
Joshua MacSween <joshmacsween@gmail.com>
2019-10-16 12:58:27 -0600

    removed save! from create

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -25,7 +25,7 @@ class WorkoutsController < ApplicationController
   # POST /workouts
   # POST /workouts.json
   def create
-    @workout = current_user.workouts.build(workout_params).save!
+    @workout = current_user.workouts.build(workout_params)
 
     respond_to do |format|
       if @workout.save
