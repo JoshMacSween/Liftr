51499c875a883b8bc60fed69bfc0613ffb88b530
Joshua MacSween <joshmacsween@gmail.com>
2019-10-16 12:40:38 -0600

    user_id in params

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -70,6 +70,6 @@ class WorkoutsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def workout_params
-      params.require(:workout).permit(:title, :date, exercises_attributes: [:id, :weight, :movement, :sets, :reps])
+      params.require(:workout).permit(:title, :date, user_attributes: [:id],  exercises_attributes: [:id, :weight, :movement, :sets, :reps])
     end
 end
